e9e04a6dbe66b41f952ffae901538d9edf4d88a8
ecv <ecv@ECV-BO-B018-IM040.local>
2018-06-15 10:38:29 +0200

    Add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+'use strict';
+module.exports = function(grunt) {
+     require("load-grunt-tasks")(grunt);
+     grunt.initConfig({
+          watch: {
+               sass: {
+                    files: ['css/sass/**/**/**.{scss,sass}'],
+                    tasks: ['sass:dist', 'postcss:dist']
+               },
+               'babel': {
+                    files: ['js/ecmascript.js'],
+                    tasks: ['babel:dist']
+               },
+               'uglify': {
+                    files: ['js/main.js'],
+                    tasks: ['uglify:dist']
+               }
+          },
+          sass: { // Task
+               dist: { // Target
+                    options: { // Target options
+                         style: 'compressed',
+                         compass: true
+                    },
+                    files: { // Dictionary of files
+                         'css/main.css': 'css/sass/main.scss' // 'destination': 'source',
+                    }
+               }
+          },
+          "babel": {
+               options: {
+                    sourceMap: true,
+                    presets: ['env']
+               },
+               dist: {
+                    files: {
+                         "js/main.js": "js/ecmascript.js"
+                    }
+               }
+          },
+          "uglify": {
+               dist: {
+                    files: {
+                         'js/main.js': ['js/main-min.js']
+                    }
+               }
+          },
+          postcss: {
+               options: {
+                    map: true,
+                    processors: [
+                         require('autoprefixer')({
+                              browsers: ['last 2 versions']
+                         })
+                    ]
+               },
+               dist: {
+                    src: 'css/main.css'
+               }
+          }
+     });
+
+     grunt.loadNpmTasks('grunt-contrib-sass'); // https://github.com/gruntjs/grunt-contrib-sass
+     grunt.loadNpmTasks('grunt-postcss'); // https://github.com/postcss/autoprefixer
+     grunt.loadNpmTasks('grunt-contrib-watch'); // https://github.com/gruntjs/grunt-contrib-watch
+     grunt.loadNpmTasks('grunt-contrib-uglify');
+     grunt.registerTask('default', ['sass:dist', 'babel:dist', 'uglify:dist', 'postcss:dist', 'watch']);
+};
