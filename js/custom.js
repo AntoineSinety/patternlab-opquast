f0e0b3058f238b5f0f1950045a75f2fa884c2ad8
ArthurPerrin <arthur.perrin5@orange.fr>
2018-06-15 16:49:37 +0200

    Section full page / start footer

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+        navigation: true,
+    });
+
+    $('#fp-nav li a span').each(function(id, el){
+        if (id <= 9){
+            id = "0" + (id + 1);
+        }else{
+            id = id +1;
+        }
+        $(this).html( id );
+
+    });
+
+    
+    // $('.arrowUp').click(function(){
+    //     $.fn.fullpage.moveSectionUp();
+    //     console.log('antoine');
+    // });
+    
+    // $('.arrowDown').click(function(){
+    //     $.fn.fullpage.moveSectionDown();
+    // });
+});
